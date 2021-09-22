c7e4984772f1d723631d076c92340e451085e482
Lenur Azizov <lenur29azizov@gmail.com>
2021-09-22 13:41:36 +0300

    Updates

---
+++ precache-manifest.2f935c231edf9d6f8285afacc6516bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f403d40289e38bc5bf77cd0c866c1a50",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1b4ed8e12ca68025f32",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b4ed8e12ca68025f32",
+    "url": "./static/js/main.953415a1.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
